ed08f8b594871a546fa533b00bd5159a5f3c8418
rijil <rijil@qburst.com>
2019-07-22 15:15:05 +0530

    basic

---
+++ main.js
@@ -0,0 +1,19 @@
+const displayedImage = document.querySelector(".displayed-image");
+const thumbBar = document.querySelector(".thumbnail");
+
+for(var i = 1; i <= 5; i++) {
+    var divImage = document.createElement('div')
+    divImage.className = "thumb-image";
+    var newImage = document.createElement('img');
+    var imageLocation='img/pics/pic' + i + '.jpg';
+    newImage.setAttribute('src', 'img/pics/pic' + i + '.jpg');
+    divImage.appendChild(newImage);
+    thumbBar.appendChild(divImage);
+    newImage.onclick = function(e) {
+        var imgSrc = e.target.getAttribute('src');
+        displayImage(imgSrc);
+    }
+}
+function displayImage(imgSrc) {
+    displayedImage.setAttribute('src', imgSrc);
+  }
