cc45d74a97939323c77d070352a7d6c355f0dc7c
rijil <rijil@qburst.com>
2019-07-24 18:33:53 +0530

    beta

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const displayedImage = document.querySelector(".displayed-image");
 const thumbBar = document.querySelector(".thumbnail");
-
-for(var i = 1; i <= 9; i++) {
+for(var i = 1; i <= Object.keys(path.pic).length; i++) {
     var divImage = document.createElement('div')
-    divImage.className = "thumb-image";
+    divImage.className = "thumb-image waves-effect";
     var newImage = document.createElement('img');
-    var imageLocation='img/pics/pic' + i + '.jpg';
-    newImage.setAttribute('src', 'img/pics/pic' + i + '.jpg');
+    var imageLocation=path.pic[i];
+    newImage.setAttribute('src', path.pic[i]);
     divImage.appendChild(newImage);
     thumbBar.appendChild(divImage);
     newImage.onclick = function(e) {
@@ -17,4 +16,3 @@ for(var i = 1; i <= 9; i++) {
 function displayImage(imgSrc) {
     displayedImage.setAttribute('src', imgSrc);
   }
-
